99017901b8565565169e94b79b33029aee670be1
Merlin <prasadme@tcd.ie>
2021-12-19 18:37:25 +0000

    removed old javascript files

--- visualise/graph.js
+++ visualise/graph.js
@@ -1,48 +1,20 @@
-// set the dimensions and margins of the graph
-var margin = {top: 30, right: 30, bottom: 70, left: 60},
-    width = 460 - margin.left - margin.right,
-    height = 400 - margin.top - margin.bottom;
+const CHART = document.getElementById('lineChart');
 
-// append the svg object to the body of the page
-var svg = d3.select("#my_dataviz")
-  .append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-  .append("g")
-    .attr("transform",
-          "translate(" + margin.left + "," + margin.top + ")");
-
-// Parse the Data - we'l get csv format data from data.csv
-d3.csv("http://localhost:8000/commits.csv", function(data) {
-
-// X axis
-var x = d3.scaleBand()
-  .range([ 0, width ])
-  .domain(data.map(function(d) { return d.Repo; }))
-  .padding(0.2);
-svg.append("g")
-  .attr("transform", "translate(0," + height + ")")
-  .call(d3.axisBottom(x))
-  .selectAll("text")
-    .attr("transform", "translate(-10,0)rotate(-45)")
-    .style("text-anchor", "end");
-
-// Add Y axis
-var y = d3.scaleLinear()
-  .domain([0, 60])
-  .range([ height, 0]); 
-svg.append("g")
-  .call(d3.axisLeft(y));
-
-// Bars
-svg.selectAll("mybar")
-  .data(data)
-  .enter()
-  .append("rect")
-    .attr("x", function(d) { return x(d.Repo); })
-    .attr("y", function(d) { return y(d.Commits); })
-    .attr("width", x.bandwidth())
-    .attr("height", function(d) { return height - y(d.Commits); })
-    .attr("fill", "#69b3a2")
-
+var lineChart = new Chart(CHART, {
+   type: 'line',
+   data: {
+      labels: Object.keys(data),
+      datasets: [{
+         label: 'My first dataset',
+         fill: false,
+         lineTension: 0,
+         backgroundColor: "rgba(75,192,192,0.4)",
+         borderColor: "rgba(75,192,192,1)",
+         borderCapStyle: 'butt',
+         borderDash: [],
+         borderDashOffset: 0.0,
+         borderJointStyle: 'miter',
+         data: Object.values(data)
+      }]
+   }
 })
