e444c9781267dd5ff9b322fa48f8214cf3c9bc27
Merlin <prasadme@tcd.ie>
2021-12-20 04:34:00 +0000

    added languages to tooltip

--- visualise/graph.js
+++ visualise/graph.js
@@ -9,7 +9,20 @@ function makeChart(repos) {
     return d.Commits;
   });
 
-  var languages = repos.map(function   (d){
+  
+  let sum = 0;
+  for(let i = 0 ; i < commitsData.length ; i ++) {
+    console.log(commitsData[i]);
+    sum += Number(commitsData[i]);
+  }
+
+console.log(sum);
+
+  var languages = repos.map(function (d) {
+    return d.Language;
+  });
+
+  var languagesColor = repos.map(function   (d){
    
      if (d.Language === "Java")
        return "#b07219";
@@ -38,17 +51,6 @@ function makeChart(repos) {
 
   });
 
-// /**
-//    * function to generate random color in hex form
-//    */
-//  function getRandomColorHex() {
-//   var hex = "0123456789ABCDEF",
-//       color = "#";
-//   for (var i = 1; i <= 6; i++) {
-//     color += hex[Math.floor(Math.random() * 16)];
-//   }
-//   return color;
-// }
 
 
 
@@ -58,37 +60,82 @@ function makeChart(repos) {
 //create a chart with the data collected
   var chart = new Chart("myChart", {
     type: "bar",
-    options: {
-      maintainAspectRatio: false,
-      legend: {
-        display: false
-      },
-      scales: {
-        yAxes: [
-          {
-            scaleLabel: {
-              display: true,
-              labelString: "Total commits",
-              fontSize: 16
-            }
-          }
-        ]
-      }
-    },
     data: {
       labels: reposLabels,
       datasets: [
         {
-          label: "Total commits per repository",
-          backgroundColor: languages,
+          label: "yo",
+          backgroundColor: languagesColor,
           data: commitsData, 
           borderWidth: 1
         }
+        
       ]
     },
     
+      options: {
+        responsive : true,
+        maintainAspectRatio: true,
+        legend: {
+           position: 'bottom',
+           display:  false  //fix this later 
+        },
+        title: {
+          display: true,  
+          text: "Total commits per repository"
+        },
+        scales: {
+          yAxes: [
+            {
+              scaleLabel: {
+                display: true,
+                labelString: "Total commits",
+                fontSize: 16
+              }
+            }
+          ]
+        },
 
-    
+         // animation: true,
+    // animationSteps: 60,
+
+        // onClick: function(c,i){
+        //     e = i[0];
+        //   console.log("index",e._index)
+        //   var x_value = this.data.labels[e._index];
+        //   var y_value = this.data.datasets[0].data[e._index];
+        //   console.log("x value",x_value);
+        //   console.log("y value",y_value);
+        //   },
+       
+        tooltips: {
+          displayColors:false,
+          callbacks: {
+            label: function(tooltipItem, data) {
+
+              let line1 = "commits: " + Number(tooltipItem.yLabel)  ;
+            
+
+              var numerator = commitsData[tooltipItem.index]
+              var denominator =   sum ;
+              var percent = Math.round((numerator/denominator) * 100)
+              console.log(numerator)
+
+              let line2 =  "percentage: " + percent + "%";
+
+              return  [line1 ,line2];
+            },
+            afterLabel: function(tooltipItem, data) {
+     
+            
+
+              return  "language: " + languages[tooltipItem.index] ; 
+
+             
+            }
+          }
+        }
+      }
   });
 
 }
